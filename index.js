bd21afdf82f403355877d12396676f5be605343f
kempsjerome <degresave@gmail.com>
2023-11-07 13:48:32 -0800

    ajou des fichier calculatrice

---
+++ index.js
@@ -0,0 +1,50 @@
+function addition(){
+    let n1 =  document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1)
+    let numero2 = parseFloat(n2)
+
+    let addition = numero1 + numero2
+    window.alert("le resultat de l addition est :" + addition)
+     
+
+}
+
+function division(){
+    let n1 =  document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1)
+    let numero2 = parseFloat(n2)
+
+    let division = numero1 / numero2
+    window.alert("le resultat de la Division est :" + division)
+     
+
+}
+
+function multiplication(){
+    let n1 =  document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1)
+    let numero2 = parseFloat(n2)
+
+    let multiplication = numero1 * numero2
+    window.alert("le resultat de la Multiplication est :" + multiplication)
+     
+
+
+}
+function soustraction(){
+    let n1 =  document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1)
+    let numero2 = parseFloat(n2)
+
+    let soustraction = numero1 * numero2
+    window.alert("le resultat de la Soustraction est :" + soustraction)
+     
+}
